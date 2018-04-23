2bfb04e02431199f1f1eab239a7fefbc86515353
Maxor <gobole20@gmail.com>
2018-04-23 14:08:43 +0200

    Compteur incrémenté

--- app/src/main/java/com/sudoku/maxor/sudoku/MainActivity.java
+++ app/src/main/java/com/sudoku/maxor/sudoku/MainActivity.java
@@ -2,12 +2,35 @@ package com.sudoku.maxor.sudoku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvNumb;
+    private Button btnPlus;
+    private int chiffre;
+    private String test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvNumb = (TextView) findViewById(R.id.tvNumb);
+        btnPlus = (Button) findViewById(R.id.btnPlus);
+        btnPlus.setOnClickListener(btnclick);
+        chiffre = 0;
+
     }
+
+
+    private View.OnClickListener btnclick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            chiffre = chiffre + 1;
+            tvNumb.setText("Nombre : " + String.valueOf(chiffre));
+
+        }
+
+    };
 }
